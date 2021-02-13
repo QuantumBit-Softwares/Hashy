4bfe49956c54ef2374b48fe107895ff50d80489d
Asher <51806917+asher-lab@users.noreply.github.com>
2021-02-14 00:03:26 +0800

    Update run.py

--- DatabaseProgram/run.py
+++ DatabaseProgram/run.py
@@ -1,5 +1,5 @@
 from subprocess import call
-call(["python3", "Install.py"])
+#call(["python3", "Install.py"])
 call(["python3", "Main.py"])
-call(["python3", "MostPopularTweets.py"])
-call(["python3", "convert.py"])
+#call(["python3", "MostPopularTweets.py"])
+#call(["python3", "convert.py"])
