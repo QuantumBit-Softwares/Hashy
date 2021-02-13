cc941b5f3351d49a3cdf5ebbd0c6c3eb559acb0c
Asher <51806917+asher-lab@users.noreply.github.com>
2021-02-13 13:33:22 +0000

    Update pptoconvert.py

--- Database/Python-Json-to-Sql/PrettyTableConversion/pptoconvert.py
+++ Database/Python-Json-to-Sql/PrettyTableConversion/pptoconvert.py
@@ -1,37 +1,49 @@
-input_string = """+-----------+-------+
-| Word      | Count |
-+-----------+-------+
-| the       |   107 |
-| RT        |    83 |
-| #GameStop |    72 |
-| and       |    49 |
-| to        |    46 |
-| a         |    40 |
-| of        |    38 |
-| is        |    36 |
-| on        |    28 |
-| in        |    25 |
-+-----------+-------+
-"""
 
-import re, csv, sys
+
+#################################################
+# Importing modules
+import re 
+import tweepy 
+from tweepy import OAuthHandler 
+from textblob import TextBlob 
+from prettytable import PrettyTable
+import json
+from cherrypicker import CherryPicker
+import pandas as pd
+import twitter
+import csv_to_sqlite
+import csv, sys
+import os
+from subprocess import call
+
+
+#############################################
 def pretty_table_to_tuples(input_str):
-    lines = input_str.split("\n")
-    num_columns = len(re.findall("\+", lines[0])) - 1
-    line_regex = r"\|" + (r" +(.*?) +\|"*num_columns)
-    for line in lines:
-        m = re.match(line_regex, line.strip())
-        if m:
-            yield m.groups()
-
-
-
-with open('output.csv', 'w') as outcsv:
+        lines = input_str.split("\n")
+        num_columns = len(re.findall("\+", lines[0])) - 1
+        line_regex = r"\|" + (r" +(.*?) +\|"*num_columns)
+        for line in lines:
+            m = re.match(line_regex, line.strip())
+            if m:
+                yield m.groups()
+            
+#with open('Dumps/Freq_Words.txt') as fp:
+fp = open('Dumps/Freq_Words.txt')
+input_string = fp.read()
+with open('CSV/Freq_Words.csv', 'w') as outcsv:
     writer = csv.writer(outcsv)
     writer.writerows(pretty_table_to_tuples(input_string))
-
-#w = csv.writer(sys.stdout)
-#w.writerows(pretty_table_to_tuples(input_string))
-
-print("\n\n\n")
-#print(x)
+    
+#with open('Dumps/ScreenNames.txt') as fp:    
+fp = open('Dumps/ScreenNames.txt')
+input_string = fp.read()
+with open('CSV/ScreenNames.csv', 'w') as outcsv:
+    writer = csv.writer(outcsv)
+    writer.writerows(pretty_table_to_tuples(input_string))
+    
+#with open('Dumps/related_hashtags_with_count.txt') as fp:
+fp = open('Dumps/related_hashtags_with_count.txt')
+input_string = fp.read()
+with open('CSV/related_hashtags_with_count.csv', 'w') as outcsv:
+    writer = csv.writer(outcsv)
+    writer.writerows(pretty_table_to_tuples(input_string))
